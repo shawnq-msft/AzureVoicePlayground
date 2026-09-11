e73668df16c529fe4aaa83afd52b0ea9779a8efa
Shawn Qiu <qiushuo@live.com>
2026-09-11 17:40:26 +0800

    Match Voice Live architecture to aero design

--- src/buildTimestamp.ts
+++ src/buildTimestamp.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated during build
-export const BUILD_TIMESTAMP = '2026-09-11T09:36:09.268Z';
-export const BUILD_COMMIT = 'a07f4aa';
+export const BUILD_TIMESTAMP = '2026-09-11T09:39:06.159Z';
+export const BUILD_COMMIT = '032c9ba';
